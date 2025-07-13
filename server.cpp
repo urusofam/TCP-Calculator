e0ca099007b8acae0a59c5b84e5820144a3f34c0
urusofam <urusofam@gmail.com>
2025-07-13 13:12:22 +0300

    add main server file

---
+++ server.cpp
@@ -0,0 +1,24 @@
+//
+// Created by urus on 13.07.25.
+//
+
+#include "TCPServer.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+        return 1;
+    }
+    int port = std::stoi(argv[1]);
+
+    try {
+        TCPServer server(port);
+        server.run();
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Server error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
